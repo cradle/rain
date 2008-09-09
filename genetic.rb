98fbbaf099924c6dfba9d326e98d4df5e71b4a82
Glenn Murray <cradle@gmail.com>
2008-09-10 00:25:41 +1000

    Genetic algorithm. Finds equations which equal 42. Based on http://www.ai-junkie.com/ga/intro/gat1.html

---
+++ genetic.rb
@@ -0,0 +1,86 @@
+class Equation
+  attr_accessor :genes
+
+  def self.generate(number = 100)
+    (0..number).collect do
+      Equation.new
+    end
+  end
+
+  def self.naturally_select(collection)
+    total_fitness = collection.collect {|c| c.fitness}.inject {|a,b|a+b}
+    roulette_arrow = rand * total_fitness
+    collection.each do |c|
+      roulette_arrow -= c.fitness
+      return c if roulette_arrow <= 0
+    end
+    return nil
+  end
+
+  def self.select_perfect(collection)
+    collection.select {|c| c.fitness == 1/0.0 }
+  end
+
+  def self.next_generation(collection)
+    (0..collection.size).collect do
+      naturally_select(collection).breed(naturally_select(collection))
+    end
+  end
+
+  
+  def initialize(genes = nil)
+    @genes = genes || (0..(NUMBER_OF_GENES*GENE_LENGTH)).collect do
+      rand(2) 
+    end.join
+  end
+
+  def <=>(other)
+    fitness <=> other.fitness
+  end
+  
+
+  NUMBER_OF_GENES = 9
+  GENE_LENGTH = 4
+  TRANSLATE = {
+    "0000" => 0,
+    "0001" => 1,
+    "0010" => 2,
+    "0011" => 3,
+    "0100" => 4,
+    "0101" => 5,
+    "0110" => 6,
+    "0111" => 7,
+    "1000" => 8,
+    "1001" => 9,
+    "1010" => :"+",
+    "1011" => :"-",
+    "1100" => :"*",
+    "1101" => :"/",
+  }
+
+  def equation
+    string = (0...NUMBER_OF_GENES).collect do |i| 
+      TRANSLATE[@genes[i*GENE_LENGTH...(i+1)*GENE_LENGTH]]
+    end.join
+  end
+  
+  def evaluate
+    begin
+      eval(equation).to_f
+    rescue SyntaxError, NoMethodError, TypeError, ZeroDivisionError
+    end
+  end
+
+  def fitness
+    if evaluate == nil
+      0
+    else
+      (1 / (42.0 - evaluate)).abs
+    end
+  end
+
+  def breed(other)
+    slice_point = rand(GENE_LENGTH*NUMBER_OF_GENES)
+    Equation.new(@genes[0..slice_point] + other.genes[(slice_point+1)..-1])
+  end
+end
